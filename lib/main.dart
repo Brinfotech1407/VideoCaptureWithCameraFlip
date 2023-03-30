ac4ac535391ce07c7e6f2125936447aff4d01eed
Ramu Pal <ramu.pal2603@gmail.com>
2023-03-30 22:02:24 +0530

    Fixed flip issue and removed header bar and bottom navigation bar.

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@
 import 'package:flutter/material.dart';
 import 'package:video_recording_app/video_recorder_app.dart';
 
+import 'camera_example_app.dart';
+
 
 
 void main() {
-  runApp(VideoRecorderExample());
+  runApp(const MaterialApp(home: VideoRecorderExample()));
 }
 
 
