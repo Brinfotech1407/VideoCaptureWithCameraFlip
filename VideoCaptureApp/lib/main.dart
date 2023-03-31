1058c25fc4b662066c87860c5096903020aed6dc
Ramu Pal <ramu.pal2603@gmail.com>
2023-03-31 13:21:58 +0530

    Initial Commit Feature camera flip.

---
+++ VideoCaptureApp/lib/main.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+import 'package:video_recording_app/video_recorder_app.dart';
+
+import 'camera_example_app.dart';
+
+
+
+void main() {
+  runApp(const MaterialApp(home: VideoRecorderExample()));
+}
+
+
+
+
