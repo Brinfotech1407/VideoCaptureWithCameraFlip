39331804d1e8b557219df62b59485cbc86cdecd1
Ramu Pal <ramu.pal2603@gmail.com>
2023-04-02 19:39:23 +0530

    Fixed UI.

--- VideoCaptureApp/lib/upload/upload_media.dart
+++ VideoCaptureApp/lib/upload/upload_media.dart
@@ -3,11 +3,13 @@ import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
-import 'package:video_recording_app/upload/button_widget.dart';
 import 'package:video_recording_app/upload/firebase_api.dart';
+
 class MediaUploadView extends StatefulWidget {
   MediaUploadView(this.mediaPath, {super.key});
+
   String mediaPath;
+
   @override
   _MediaUploadViewState createState() => _MediaUploadViewState();
 }
@@ -68,20 +70,28 @@ class _MediaUploadViewState extends State<MediaUploadView> {
   }
 
   Widget buildUploadStatus(UploadTask task) => StreamBuilder<TaskSnapshot>(
-    stream: task.snapshotEvents,
-    builder: (context, snapshot) {
-      if (snapshot.hasData) {
-        final snap = snapshot.data!;
-        final progress = snap.bytesTransferred / snap.totalBytes;
-        final percentage = (progress * 100).toStringAsFixed(2);
+        stream: task.snapshotEvents,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final snap = snapshot.data!;
+            final progress = snap.bytesTransferred / snap.totalBytes;
+            final percentage = (progress * 100).toStringAsFixed(2);
 
-        return Text(
-          '$percentage %',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        );
-      } else {
-        return Container();
-      }
-    },
-  );
+            return Column(
+              children: [
+                Center(child: CircularProgressIndicator()),
+                Container(
+                  margin: EdgeInsets.only(top: 15),
+                  child: Text(
+                    '$percentage %',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return Container();
+          }
+        },
+      );
 }
